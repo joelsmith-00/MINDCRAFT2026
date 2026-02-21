0bd576a4d0d71cd1092ad6e96d8dad15927232d4
joelsmith-00 <mail-js96292006@gmail.com>
2026-02-21 11:48:13 +0530

    Clean repo: keep only assistant_ai.py and assistant_test.py

--- dotdev_club/android/app/src/main/kotlin/com/dotdev/dotdev_club/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.dotdev.dotdev_club
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- space_shooter_game/android/app/src/main/kotlin/com/spaceshooter/space_shooter_game/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.spaceshooter.space_shooter_game
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
